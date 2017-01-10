a515b9ff93df5ebc90d9e2269aa71ddc18e93899
Jin & Katie O'Neill <pair+jindigiordano.katieboundary@devbootcamp.com>
2017-01-10 15:42:22 -0800

    Ability to add class to turn preview italic

--- js/application.js
+++ js/application.js
@@ -5,5 +5,27 @@ $(document).ready(function(){
 
 
 function transform() {
-  $("#preview").html($("textarea").val());
+  var input = $("textarea").val();
+  //regex
+  var italic1 = /[*].+[*]/;
+  var italic2 = /[_].+[_]/;
+  var bold = /[*]{2}.+[*]{2}/;
+
+
+  // var matches = input.match(italic)
+  // matches.addClass("italic");
+
+  $("#preview").html("<span>" + input + "</span>");
+
+  $("span").addClass("italic");
 }
+
+
+
+
+
+
+
+// pseudocode:
+
+// $( "div" ).addClass("italic")
