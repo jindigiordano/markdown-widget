e22c0f56f62bc7ad56918fe81445f0ee76c9121f
Jin & Katie O'Neill <pair+jindigiordano.katieboundary@devbootcamp.com>
2017-01-10 16:27:49 -0800

    Can transform a string with one instance each of bold, italic, and underscore italic

--- js/application.js
+++ js/application.js
@@ -12,12 +12,24 @@ function transform() {
   var italic2 = /[_].+[_]/;
   var bold = /[*]{2}.+[*]{2}/;
 
-  var italics = input.match(italic1);
 
 
-  if (italics !== null) {
-    output = input.replace(/[*].+[*]/, "<span class = 'italic'>" + italics[0].slice(1, -1) + "</span>");
+  var bolds = input.match(bold);
+
+  if (bolds !== null) {
+    output = output.replace(bold, "<span class = 'bold'>" + bolds[0].slice(2, -2) + "</span>");
   }
+
+  var italics = output.match(italic1);
+  if (italics !== null) {
+    output = output.replace(italic1, "<span class = 'italic'>" + italics[0].slice(1, -1) + "</span>");
+  }
+
+  var italics2 = output.match(italic2);
+  if (italics2 !== null) {
+    output = output.replace(italic2, "<span class = 'italic'>" + italics2[0].slice(1, -1) + "</span>");
+  }
+
 
   $("#preview").html(output);
 
